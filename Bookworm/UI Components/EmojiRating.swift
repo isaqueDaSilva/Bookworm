e92fa11f03a77962914b62a0c7383331dd81bd5c
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-08-31 23:11:11 -0300

    AddNewBookView complete

---
+++ Bookworm/UI Components/EmojiRating.swift
@@ -0,0 +1,27 @@
+//
+//  EmojiRating.swift
+//  Bookworm
+//
+//  Created by Isaque da Silva on 31/08/23.
+//
+
+import SwiftUI
+
+struct EmojiRating: View {
+    let rating: Int16
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("😖")
+        case 2:
+            Text("🙁")
+        case 3:
+            Text("😐")
+        case 4:
+            Text("☺️")
+        default:
+            Text("🤩")
+        }
+    }
+}
--- Bookworm/View/AddNewBookView.swift
+++ Bookworm/View/AddNewBookView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct AddNewBookView: View {
+    @Environment(\.dismiss) var dismiss
     @StateObject var viewModel = AddNewBookViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section("About the Book") {
+                    TextField("Book Title", text: $viewModel.title)
+                    TextField("Author", text: $viewModel.author)
+                    DatePicker("Release Data", selection: $viewModel.releaseDate, in: ...Date.now, displayedComponents: .date)
+                    Picker("Genre", selection: $viewModel.genre) {
+                        ForEach(viewModel.genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                Section("Review") {
+                    RatingStars(rating: $viewModel.rating)
+                    
+                    ZStack(alignment: .leading) {
+                        if viewModel.review.isEmpty {
+                            Text("What did you think about the book?")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary.opacity(0.6))
+                                .padding(.horizontal, 3)
+                        }
+                        TextEditor(text: $viewModel.review)
+                    }
+                }
+            }
+            .navigationTitle("Add New Book")
+        }
     }
 }
