aaa714dc9c6cb8b9667da7a7a232313a3b302a8e
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-08-31 23:26:12 -0300

    Finish Detail View

--- Bookworm/View/BookDetailsView.swift
+++ Bookworm/View/BookDetailsView.swift
@@ -8,8 +8,59 @@
 import SwiftUI
 
 struct BookDetailsView: View {
+    @Environment(\.dismiss) var dismiss
     @StateObject var viewModel = BookDetailsViewModel()
+    let book: Books
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.wrappedGenre)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.wrappedGenre.uppercased())
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .offset(x: -5, y: -5)
+            }
+            
+            VStack {
+                Text(book.author?.wrappedName ?? "Unknown Author")
+                    .font(.largeTitle)
+                    .foregroundColor(.secondary)
+                
+                Text("In: \(viewModel.dateFormatter.string(from: book.wrappedReleaseDate))")
+                    .font(.headline.bold())
+                
+                Text(book.wrappedReview)
+                    .font(.headline.bold())
+                    .padding()
+                
+                RatingStars(rating: .constant(Int(book.rating)))
+            }
+        }
+        .navigationTitle(book.wrappedTitle)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            Button{
+                viewModel.deleteCurrentBookAlert = true
+            } label: {
+                Label("Delete", systemImage: "trash")
+            }
+        }
+        .alert("Delete Book", isPresented: $viewModel.deleteCurrentBookAlert) {
+            Button("Delete", role: .destructive) {
+                viewModel.deleteCurrentBook(book)
+                dismiss()
+            }
+            
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Are you sure you want to delete this book?")
+        }
     }
 }
