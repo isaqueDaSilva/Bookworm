3d1ae9a667dbd3116376e83487cc7d80a19af1c7
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-12-12 23:20:39 -0300

    I modifed some things of the test for the AddNewBookViewModel

--- Bookworm/Bookworm_Tests/AddNewBookViewModel_Tests.swift
+++ Bookworm/Bookworm_Tests/AddNewBookViewModel_Tests.swift
@@ -9,12 +9,12 @@ import XCTest
 @testable import Bookworm
 
 final class AddNewBookViewModel_Tests: XCTestCase {
-    var viewModel: AddNewBookViewModel?
-    var manager = BooksMananger(path: FileManager.documentsDirectoryForTests.appending(component: "SavedTestBooks"))
-    func onSave() { print("Save with success.") }
+    private var viewModel: AddNewBookViewModel?
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        self.viewModel = AddNewBookViewModel(manager: manager, onSave: onSave)
+        
+        self.viewModel = AddNewBookViewModel()
     }
     
     override func tearDownWithError() throws {
@@ -24,12 +24,15 @@ final class AddNewBookViewModel_Tests: XCTestCase {
     
     func test_AddNewBookViewModel_isValid_shouldBeTrue() {
         // Given
-        
+        let newBook = Book.bookExemple
         // When
         guard let viewModel = viewModel else { return }
-        viewModel.title = UUID().uuidString
-        viewModel.author = UUID().uuidString
-        viewModel.review = UUID().uuidString
+        viewModel.title = newBook.title
+        viewModel.author = newBook.author.name
+        viewModel.releaseDate = newBook.releaseDate
+        viewModel.genre = newBook.genre
+        viewModel.review = newBook.review
+        viewModel.rating = newBook.rating
         
         // Than
         XCTAssertFalse(viewModel.title.isEmpty)
@@ -40,12 +43,13 @@ final class AddNewBookViewModel_Tests: XCTestCase {
     
     func test_AddNewBookViewModel_isValid_shouldBeFalse() {
         // Given
+        let newBook = Book.bookExemple
         
         // When
         guard let viewModel = viewModel else { return }
         viewModel.title = ""
-        viewModel.author = UUID().uuidString
-        viewModel.review = UUID().uuidString
+        viewModel.author = newBook.author.name
+        viewModel.review = newBook.review
         
         // Then
         XCTAssertTrue(viewModel.title.isEmpty)
