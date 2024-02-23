9b5626399392e95f65fed5b142a411f9af699054
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-02-23 04:04:23 -0300

    I created the Storage type to manipulate some actions of SwiftData.

--- Bookworm/Bookworm/Manager/Storage.swift
+++ Bookworm/Bookworm/Manager/Storage.swift
@@ -6,3 +6,78 @@
 //
 
 import Foundation
+import SwiftData
+
+final class Storage: ObservableObject {
+    let container: ModelContainer
+    let context: ModelContext
+    
+    @Published var user: User?
+    
+    private func save() throws {
+        try self.context.save()
+        try self.fetch()
+    }
+    
+    private func fetch() throws {
+        let descriptor = FetchDescriptor<User>()
+        let userFetched = try context.fetch(descriptor)
+        
+        guard let user = userFetched.first else {
+            self.user = nil
+            return
+        }
+        
+        self.user = user
+    }
+    
+    private func getUser() throws -> User {
+        guard let user = self.user else {
+            throw StorageError.dataNotFound
+        }
+        
+        return user
+    }
+    
+    func createUser(_ user: User) throws {
+        self.context.insert(user)
+        try self.save()
+    }
+    
+    func createAuthor(_ author: Author) throws {
+        let user = try self.getUser()
+        
+        if !user.authors.contains(author) {
+            user.authors.append(author)
+        } else {
+            throw StorageError.duplicateItem
+        }
+    }
+    
+    func createBook(_ book: Book) throws {
+        let user = try self.getUser()
+        
+        for author in user.authors {
+            if author.authorName == book.author {
+                if !author.books.contains(book) {
+                    author.books.append(book)
+                } else {
+                    throw StorageError.duplicateItem
+                }
+            }
+        }
+        
+        try save()
+    }
+    
+    
+    
+    init() {
+        do {
+            self.container = try ModelContainer(for: User.self)
+            self.context = ModelContext(container)
+        } catch let error {
+            fatalError(error.localizedDescription)
+        }
+    }
+}
