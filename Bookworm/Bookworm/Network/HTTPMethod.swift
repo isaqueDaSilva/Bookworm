1337bf1fb4ace90080e53a09b7282f578d4e5f60
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-02-23 03:17:11 -0300

    I created the enum with HTTP method, for use in API request.

---
+++ Bookworm/Bookworm/Network/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  Bookworm
+//
+//  Created by Isaque da Silva on 23/02/24.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
