b3ceb17e68bce650c2ddacb3b0120aa70dd2bd21
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-02-23 04:06:31 -0300

    I created the StorageError enum to handle some errors that may occur in SwiftData at run time.

--- Bookworm/Bookworm/Manager/StorageError.swift
+++ Bookworm/Bookworm/Manager/StorageError.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+enum StorageError: Error, LocalizedError, Identifiable {
+    case dataNotFound
+    case duplicateItem
+    
+    var id: String {
+        self.localizedDescription
+    }
+    
+    var errorDescription: String? {
+        switch self {
+        case .dataNotFound:
+            return NSLocalizedString("We were unable to locate the requested data, please try again.", comment: "")
+        case .duplicateItem:
+            return NSLocalizedString("This item already exists, please create a new one.", comment: "")
+        }
+    }
+}
