3e7af79d57678e48e3652dc9b0aec178eb72cead
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-02-26 04:38:32 -0300

    I set up the login view.

--- Bookworm/Bookworm/View/LoginView.swift
+++ Bookworm/Bookworm/View/LoginView.swift
@@ -8,8 +8,71 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            GeometryReader{ geo in
+                VStack {
+                    HStack(alignment: .firstTextBaseline) {
+                        Image(systemName: "book.fill")
+                        Text("Bookwork")
+                    }
+                    .font(.system(size: 40))
+                    .fontWeight(.heavy)
+                    .bold()
+                    .foregroundStyle(Color(uiColor: .systemBlue))
+                    .frame(height: geo.size.height / 4)
+                    
+                    VStack {
+                        EntryField(
+                            label: Text("Email"),
+                            content: TextField("insert your email here", text: $email),
+                            leadingAccessoryView: Image(systemName: "person")
+                        )
+                        .padding(.bottom, 20)
+                        
+                        EntryField(
+                            label: Text("Password"),
+                            content: SecureFieldWithToggle(title: "Insert your password here...", text: $password),
+                            leadingAccessoryView: Image(systemName: "key")
+                        )
+                    }
+                    .frame(height: geo.size.height / 4)
+                    .padding(.bottom, 20)
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Login")
+                            .font(.title3)
+                            .bold()
+                            .foregroundStyle(.white)
+                            .frame(maxWidth: .infinity)
+                            .frame(minHeight: 54, alignment: .center)
+                            .background {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .foregroundStyle(Color(uiColor: .systemBlue))
+                            }
+                        
+                    }
+                    .buttonStyle(.plain)
+                }
+                .padding()
+                .toolbar {
+                    ToolbarItem(placement: .bottomBar) {
+                        HStack(spacing: 0) {
+                            Text("Don't have an account?")
+                            Button("Create Account") {
+                                
+                            }
+                        }
+                        .font(.subheadline)
+                        .bold()
+                    }
+                }
+            }
+        }
     }
 }
 
