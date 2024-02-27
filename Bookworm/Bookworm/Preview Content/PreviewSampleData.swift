3cc9773d35af7112eedfda3f7db17891a2ea0569
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-02-27 00:16:25 -0300

    I created a static property to add some sample datas for use in Xcode Preview.

--- Bookworm/Bookworm/Preview Content/PreviewSampleData.swift
+++ Bookworm/Bookworm/Preview Content/PreviewSampleData.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+extension Storage {
+    static var preview: Storage = {
+        let storage = Storage(isStoredInMemoryOnly: true)
+        let context = storage.context
+        
+        let bookSample = Book(
+            id: UUID(),
+            title: "Harry Potter",
+            author: "J.K Rowlling",
+            releaseDate: "26/07/1997",
+            genre: "Fantasy",
+            review: "A good book",
+            rating: 5
+        )
+        
+        let authorSample = Author(
+            id: UUID(),
+            authorName: "J.K Rowlling",
+            books: [bookSample]
+        )
+        
+        let userSample = User(
+            id: UUID(),
+            name: "Tim Cook",
+            username: "tim_cook",
+            email: "tim.cook@apple.com",
+            author: [authorSample]
+        )
+        
+        context.insert(userSample)
+        do {
+            try context.save()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        return storage
+    }()
+}
