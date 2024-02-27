a9df8541e40f8507a27cfa51145912db8b954fb3
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-02-27 00:14:36 -0300

    I added a new case for NetworkError enumeration to handle some failure when encoding data.

--- Bookworm/Bookworm/Network/NetworkError.swift
+++ Bookworm/Bookworm/Network/NetworkError.swift
@@ -13,6 +13,7 @@ enum NetworkError: Error, LocalizedError, Identifiable {
     case invalidHeaderField
     case faliedToGetData
     case invalidResponse
+    case faliedToEncodeData
     
     var id: String {
         self.localizedDescription
@@ -30,6 +31,8 @@ enum NetworkError: Error, LocalizedError, Identifiable {
             return NSLocalizedString("Failed to receive data, please try again.", comment: "")
         case .invalidResponse:
             return NSLocalizedString("Invalid receive response, please try again.", comment: "")
+        case .faliedToEncodeData:
+            return NSLocalizedString("Unable to transform data to the correct type for sending, please try again.", comment: "")
         }
     }
 }
