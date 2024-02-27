c279be125075fda80447cbaedcf5738057b2b6af
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-02-27 00:18:09 -0300

    I created an action button for use in Login and Create Account views.

--- Bookworm/Bookworm/View/Components/ActionButton.swift
+++ Bookworm/Bookworm/View/Components/ActionButton.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct ActionButton: View {
+    let title: String
+    var isLoadingState: Bool
+    var action: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            action()
+        } label: {
+            VStack {
+                if isLoadingState {
+                    ProgressView()
+                } else {
+                    Text(title)
+                        .font(.title3)
+                        .bold()
+                        .foregroundStyle(.white)
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .frame(minHeight: 54, alignment: .center)
+            .background {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundStyle(Color(uiColor: .systemBlue))
+            }
+            
+        }
+        .buttonStyle(.plain)
     }
-}
-
-#Preview {
-    ActionButton()
 }
