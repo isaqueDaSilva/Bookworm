23e1b68b608c00b3a7a52e90806b9a89ae3ae480
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-02-27 00:21:21 -0300

    I created the Icons enumeration to store the sf symbols will be use in App.

--- Bookworm/Bookworm/View/Components/Icons.swift
+++ Bookworm/Bookworm/View/Components/Icons.swift
@@ -5,4 +5,16 @@
 //  Created by Isaque da Silva on 26/02/24.
 //
 
-import Foundation
+import SwiftUI
+
+enum Icons: String {
+    case person = "person"
+    case envelope = "envelope"
+    case key = "key"
+    case eye = "eye"
+    case eyeSlash = "eye.slash"
+    
+    var systemImage: Image {
+        Image(systemName: self.rawValue)
+    }
+}
