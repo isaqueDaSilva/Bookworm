bc600943a5e471f8ad3a0b967b2bb4b8eb41f856
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-02-27 00:20:20 -0300

    A header with the logo of the app

--- Bookworm/Bookworm/View/Components/BookwormLogo.swift
+++ Bookworm/Bookworm/View/Components/BookwormLogo.swift
@@ -5,4 +5,17 @@
 //  Created by Isaque da Silva on 26/02/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct BookwormLogo: View {
+    var body: some View {
+        HStack(alignment: .firstTextBaseline) {
+            Image(systemName: "book.fill")
+            Text("Bookworm")
+        }
+        .font(.system(size: 40))
+        .fontWeight(.heavy)
+        .bold()
+        .foregroundStyle(Color(uiColor: .systemBlue))
+    }
+}
