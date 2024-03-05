191f9b6d83c787d54ab084e46fddbadedc31bdb9
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-05 04:04:23 -0300

    I created the custom cover will be displayed at times when the user does not insert a cover image for the book.

--- Bookworm/Bookworm/View/Components/Cover.swift
+++ Bookworm/Bookworm/View/Components/Cover.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct Cover: View {
+    let title: String
+    let author: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center) {
+            Image(systemName: "book.fill")
+                .font(.system(size: 40))
+                .foregroundStyle(.blue.gradient)
+                .padding(.bottom, 5)
+            Text(title)
+                .font(.title)
+                .fontWeight(.heavy)
+                .fontDesign(.serif)
+            Text(author)
+                .font(.headline)
+                .bold()
+                .foregroundStyle(.secondary)
+        }
+        .multilineTextAlignment(.center)
+        .frame(width: 135, height: 180)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundStyle(Color(.systemGray3))
+        )
     }
 }
 
 #Preview {
-    Cover()
+    Cover(title: "Hary Potter", author: "J.K Rowlling")
 }
