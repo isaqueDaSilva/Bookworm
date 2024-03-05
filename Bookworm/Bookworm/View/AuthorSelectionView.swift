7886cb08854da0ea75a1cc54bad8de2016639b8c
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-05 04:06:46 -0300

    I created the AuthorSelectionView who is responsible for display, updating and deleting an instance of an author.

--- Bookworm/Bookworm/View/AuthorSelectionView.swift
+++ Bookworm/Bookworm/View/AuthorSelectionView.swift
@@ -8,11 +8,85 @@
 import SwiftUI
 
 struct AuthorSelectionView: View {
+    @Environment(\.dismiss) var dismiss
+    @StateObject private var viewModel: AuthorSelectionViewModel
+    
+    var action: (Author) -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(viewModel.authorList, id: \.wrappedName) { author in
+                HStack {
+                    Text(author.wrappedName)
+                    Spacer()
+                    if author == viewModel.authorSelected {
+                        Image(systemName: "checkmark")
+                            .foregroundStyle(.tint)
+                            .font(.body.weight(.semibold))
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .onTapGesture {
+                    viewModel.authorSelected = author
+                    action(author)
+                    dismiss()
+                }
+                .contextMenu {
+                    Button {
+                        alertWithTextField(
+                            title: "Update Author",
+                            message: "",
+                            text: author.wrappedName,
+                            placeholder: "Insert the author name here",
+                            primaryButtonTitle: "OK",
+                            secondaryButtonTitle: "Cancel"
+                        ) { authorName in
+                            viewModel.updateAuthor(author, authorName)
+                        }
+                    } label: {
+                        Label("Edit", systemImage: Icons.pencil.rawValue)
+                    }
+                    
+                    Button(role: .destructive) {
+                        viewModel.deleteAuthor(author)
+                    } label: {
+                        Label("Delete", systemImage: Icons.trash.rawValue)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Choice an Author")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            Button {
+                alertWithTextField(
+                    title: "New Author",
+                    message: "Create a new author to use them to register a new book.",
+                    placeholder: "Insert the author name here",
+                    primaryButtonTitle: "OK",
+                    secondaryButtonTitle: "Cancel"
+                ) { authorName in
+                    viewModel.createAuthor(authorName)
+                }
+            } label: {
+                Icons.plus.systemImage
+            }
+        }
+        .alert(viewModel.errorTitle, isPresented: $viewModel.showingError) {
+        } message: {
+            Text(viewModel.errorMessage)
+        }
+        
+    }
+    
+    init(authorSelected: Author? = nil, storage: Storage, action: @escaping (Author) -> Void) {
+        _viewModel = StateObject(wrappedValue: AuthorSelectionViewModel(storage: storage, author: authorSelected))
+        self.action = action
     }
 }
 
 #Preview {
-    AuthorSelectionView()
+    NavigationStack {
+        AuthorSelectionView(storage: .preview) { _ in }
+    }
 }
