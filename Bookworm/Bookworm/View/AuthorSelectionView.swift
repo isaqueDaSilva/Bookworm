7f3ec11c49231d3991baca5debad7fa1168aff6e
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-06 03:07:56 -0300

    I make some updates in AuthorSelectionView, like move the for each statement for the List it's self for simplify the code complexity.

--- Bookworm/Bookworm/View/AuthorSelectionView.swift
+++ Bookworm/Bookworm/View/AuthorSelectionView.swift
@@ -14,44 +14,42 @@ struct AuthorSelectionView: View {
     var action: (Author) -> Void
     
     var body: some View {
-        List {
-            ForEach(viewModel.authorList, id: \.wrappedName) { author in
-                HStack {
-                    Text(author.wrappedName)
-                    Spacer()
-                    if author == viewModel.authorSelected {
-                        Image(systemName: "checkmark")
-                            .foregroundStyle(.tint)
-                            .font(.body.weight(.semibold))
-                    }
+        List(viewModel.authorList) { author in
+            HStack {
+                Text(author.wrappedName)
+                Spacer()
+                if author == viewModel.authorSelected {
+                    Image(systemName: "checkmark")
+                        .foregroundStyle(.tint)
+                        .font(.body.weight(.semibold))
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .onTapGesture {
-                    viewModel.authorSelected = author
-                    action(author)
-                    dismiss()
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .onTapGesture {
+                viewModel.authorSelected = author
+                action(author)
+                dismiss()
+            }
+            .contextMenu {
+                Button {
+                    alertWithTextField(
+                        title: "Update Author",
+                        message: "",
+                        text: author.wrappedName,
+                        placeholder: "Insert the author name here",
+                        primaryButtonTitle: "OK",
+                        secondaryButtonTitle: "Cancel"
+                    ) { authorName in
+                        viewModel.updateAuthor(author, authorName)
+                    }
+                } label: {
+                    Label("Edit", systemImage: Icons.pencil.rawValue)
                 }
-                .contextMenu {
-                    Button {
-                        alertWithTextField(
-                            title: "Update Author",
-                            message: "",
-                            text: author.wrappedName,
-                            placeholder: "Insert the author name here",
-                            primaryButtonTitle: "OK",
-                            secondaryButtonTitle: "Cancel"
-                        ) { authorName in
-                            viewModel.updateAuthor(author, authorName)
-                        }
-                    } label: {
-                        Label("Edit", systemImage: Icons.pencil.rawValue)
-                    }
-                    
-                    Button(role: .destructive) {
-                        viewModel.deleteAuthor(author)
-                    } label: {
-                        Label("Delete", systemImage: Icons.trash.rawValue)
-                    }
+                
+                Button(role: .destructive) {
+                    viewModel.deleteAuthor(author)
+                } label: {
+                    Label("Delete", systemImage: Icons.trash.rawValue)
                 }
             }
         }
