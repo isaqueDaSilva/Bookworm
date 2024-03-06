52e0a0cb8e3b0000b511895000ea8201ecee4084
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-06 03:19:11 -0300

    I created the AnnotationViewModel for controller and handle with the fetch changes like create, update and delete some annotation

--- Bookworm/Bookworm/View Model/AnnotationListViewModel.swift
+++ Bookworm/Bookworm/View Model/AnnotationListViewModel.swift
@@ -5,4 +5,61 @@
 //  Created by Isaque da Silva on 05/03/24.
 //
 
+import CoreData
 import Foundation
+
+extension AnnotationListView {
+    final class AnnotationListViewModel: ObservableObject {
+        let storage: Storage
+        let book: Book
+        
+        @Published var showingAddAnotationView = false
+        
+        @Published var annotations = [Annotation]()
+        @Published var annotationSelected: Annotation? = nil
+        
+        @Published var alertTitle = ""
+        @Published var alertMessage = ""
+        @Published var showingAlert = false
+        
+        func showingFormView(_ annotation: Annotation? = nil) {
+            self.annotationSelected = annotation
+            self.showingAddAnotationView = true
+        }
+        
+        private func save() throws {
+            try self.storage.save()
+            self.fetchAnnotations()
+        }
+        
+        func fetchAnnotations() {
+            do {
+                let request = NSFetchRequest<Annotation>(entityName: EntityNames.annotation.rawValue)
+                self.annotations = try storage.context.fetch(request)
+            } catch let error {
+                self.alertTitle = "Falied to Fetch Annotations"
+                self.alertMessage = error.localizedDescription
+                self.showingAlert = true
+            }
+        }
+        
+        func deleteAnnotation(_ annotation: Annotation) {
+            do {
+                self.storage.context.delete(annotation)
+                try self.save()
+                
+            } catch let error {
+                self.alertTitle = "Falied to delete the annotation."
+                self.alertMessage = error.localizedDescription
+                self.showingAlert = true
+            }
+        }
+        
+        init(storage: Storage, book: Book) {
+            self.storage = storage
+            self.book = book
+            
+            self.fetchAnnotations()
+        }
+    }
+}
