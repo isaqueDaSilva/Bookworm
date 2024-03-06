882c10891afe3cef5a3e0218e55d97736e893a13
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-06 03:05:41 -0300

    I created the BookDetailView for display more informations about the books and the user's inputs like rating, review and end date of reading.

--- Bookworm/Bookworm/View/BookDetailView.swift
+++ Bookworm/Bookworm/View/BookDetailView.swift
@@ -8,11 +8,117 @@
 import SwiftUI
 
 struct BookDetailView: View {
+    @StateObject private var viewModel: BookDetailViewModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                Cover(
+                    title: viewModel.title,
+                    author: viewModel.author
+                )
+            }
+            .frame(maxWidth: .infinity, alignment: .center)
+            .listRowBackground(Color.listLightGray)
+            
+            Section("More Information") {
+                LabeledContent("Title:") {
+                    Text(viewModel.title)
+                }
+                
+                LabeledContent("Author:") {
+                    Text(viewModel.author)
+                }
+                
+                LabeledContent("Release Date:") {
+                    Text(viewModel.releaseDate)
+                }
+                
+                LabeledContent("Genre:") {
+                    Text(viewModel.genre)
+                }
+            }
+            
+            Section("Reading Information") {
+                LabeledContent("Stating of Reading:") {
+                    Text(viewModel.startOfReading)
+                }
+                if viewModel.isFinished {
+                    LabeledContent("End of Reading:") {
+                        Text(viewModel.endOfReading)
+                    }
+                }
+            }
+            
+            if viewModel.isFinished {
+                Section("Review") {
+                    LabeledContent("Rating:") {
+                        RatingStars(rating: .constant(viewModel.rating))
+                    }
+                    
+                    LabeledContent("Review:") {
+                        Text(viewModel.review)
+                    }
+                }
+            }
+            
+            Section {
+                Button {
+                    viewModel.showingAnnotationView = true
+                } label: {
+                    HStack {
+                        LabeledContent("Annotations") {
+                            Text(viewModel.annotationsCount, format: .number)
+                            Icons.chevronRight.systemImage
+                        }
+                    }
+                }
+                .buttonStyle(.plain)
+            }
+            
+            Section {
+                Button(role: .destructive) {
+                    viewModel.displayDeleteAlert()
+                } label: {
+                    Label("Delete Book", systemImage: Icons.trash.rawValue)
+                        .foregroundStyle(.red)
+                }
+            }
+        }
+        .navigationTitle("Details")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            Button("Edit") {
+                viewModel.showingEditView = true
+            }
+        }
+        .sheet(isPresented: $viewModel.showingEditView) {
+            BookFormView(storage: viewModel.storage, book: viewModel.book) {
+                viewModel.fetchChanges()
+            }
+        }
+        .alert(viewModel.alertTitle, isPresented: $viewModel.showingAlert) {
+            if viewModel.isDeletingAlert {
+                Button("Cancel", role: .cancel) { }
+                
+                Button("OK", role: .destructive) {
+                    viewModel.deleteBook()
+                }
+            }
+        } message: {
+            Text(viewModel.alertMessage)
+        }
+        .navigationDestination(isPresented: $viewModel.showingAnnotationView) {
+            AnnotationListView(storage: viewModel.storage, book: viewModel.book)
+        }
+    }
+    
+    init(storage: Storage, book: Book) {
+        _viewModel = StateObject(wrappedValue: BookDetailViewModel(storage: storage, book: book))
     }
 }
 
 #Preview {
-    BookDetailView()
+    NavigationStack {
+        BookDetailView(storage: .preview, book: dummyBook)
+    }
 }
