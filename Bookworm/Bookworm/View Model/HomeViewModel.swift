24c6d29bf6f18339a5f36d54349491fb0b908dce
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-06 03:13:12 -0300

    I made some changes into code for reduce some complexity and make this more readable.

--- Bookworm/Bookworm/View Model/HomeViewModel.swift
+++ Bookworm/Bookworm/View Model/HomeViewModel.swift
@@ -1,31 +1,51 @@
 //
-//  BooksViewModel.swift
+//  HomeViewModel.swift
 //  Bookworm
 //
 //  Created by Isaque da Silva on 27/02/24.
 //
 
+import CoreData
 import Foundation
 
 extension HomeView {
-    final class BooksViewModel: ObservableObject {
+    final class HomeViewModel: ObservableObject {
+        var storage: Storage
+        
+        @Published var books = [Book]()
         @Published var errorTitle = ""
         @Published var errorMessage = ""
         @Published var showingError = false
         @Published var showingAddNewBook = false
         
-        func deleteBook(
-            _ book: Book,
-            _ completion: @escaping (Book) throws -> Void
-        ) {
+        func fetchBooks() {
+            do {
+                let request = NSFetchRequest<Book>(entityName: EntityNames.book.rawValue)
+                self.books = try storage.context.fetch(request)
+            } catch let error {
+                self.errorTitle = "Falied to Fetch Books"
+                self.errorMessage = error.localizedDescription
+                self.showingError = true
+            }
+        }
+        
+        func deleteBook(_ book: Book) {
             do {
                 book.isDisabled = true
-                try completion(book)
+                storage.context.delete(book)
+                try storage.save()
+                self.fetchBooks()
             } catch let error {
                 self.errorTitle = "Falied to Delete Book"
                 self.errorMessage = error.localizedDescription
                 self.showingError = true
             }
+        }
+        
+        init(storage: Storage) {
+            self.storage = storage
+            
+            self.fetchBooks()
         }
     }
 }
