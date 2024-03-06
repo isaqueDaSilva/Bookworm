2c7639f3a111299747370cda7f68b712c96ce170
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-06 03:10:29 -0300

    This view was created for create and update some annotation about the book.

--- Bookworm/Bookworm/View/AnnotationFormView.swift
+++ Bookworm/Bookworm/View/AnnotationFormView.swift
@@ -8,11 +8,76 @@
 import SwiftUI
 
 struct AnnotationFormView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @StateObject private var viewModel: AnnotationFormViewModel
+    
+    var action: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Insert the title here...", text: $viewModel.title)
+                    TextField("Insrt the comment here...", text: $viewModel.commentDescription, axis: .vertical)
+                        .lineLimit(10, reservesSpace: true)
+                } footer: {
+                    Text("Last Modification: \(viewModel.lastUpdate)")
+                }
+                
+                if viewModel.isEditMode {
+                    Section {
+                        Button(role: .destructive) {
+                            viewModel.deleteAnnotation {
+                                action()
+                                dismiss()
+                            }
+                        } label: {
+                            Label("Delete Annotation", systemImage: Icons.trash.rawValue)
+                                .foregroundStyle(.red)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Details")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        HStack {
+                            Icons.chevronLeft.systemImage
+                            Text("Back")
+                        }
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        viewModel.saveChanges {
+                            action()
+                            dismiss()
+                        }
+                    } label: {
+                        Text("Save")
+                    }
+                }
+            }
+        }
+    }
+    
+    init(
+        storage: Storage,
+        annotation: Annotation? = nil,
+        book: Book,
+        action: @escaping () -> Void
+    ) {
+        _viewModel = StateObject(wrappedValue: AnnotationFormViewModel(storage: storage, annotation: annotation, book: book))
+        self.action = action
     }
 }
 
 #Preview {
-    AnnotationFormView()
+    AnnotationFormView(storage: .preview, book: dummyBook) { }
 }
