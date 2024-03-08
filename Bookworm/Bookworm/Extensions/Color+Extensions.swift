b9a6f743422e677e8cfedf17f8072d69fab99002
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-08 03:19:06 -0300

    I added a new extension for Color Type, for store the "listLightGray" property for use this color into some section to matching color background of the list/form.

---
+++ Bookworm/Bookworm/Extensions/Color+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Extensions.swift
+//  Bookworm
+//
+//  Created by Isaque da Silva on 08/03/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static var listLightGray = Color(CGColor(red: 246, green: 240, blue: 240, alpha: 0))
+}
