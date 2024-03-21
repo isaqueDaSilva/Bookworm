901a8ef96876787c81af881654bb340648334d9f
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-20 23:10:38 -0300

    Updated the "date String" method to read the user's current location, based on the settings, and return the date template corresponding to the one used by the user.

--- Bookworm/Bookworm/Extensions/Date+Extensions.swift
+++ Bookworm/Bookworm/Extensions/Date+Extensions.swift
@@ -11,8 +11,12 @@ extension Date {
     /// Transform a Date type in a String representation of the raw value.
     /// - Returns: Returns a String value representation about the Date type.
     func dateString() -> String {
+        let currentLocale = Locale.current
+        let template = "YYYY/MM/dd"
+        let dateFormat = DateFormatter.dateFormat(fromTemplate: template, options: 0, locale: currentLocale) ?? template
+        
         let formatter = DateFormatter()
-        formatter.dateFormat = "YY/MM/dd"
+        formatter.dateFormat = dateFormat
         
         return formatter.string(from: self)
     }
---
+++ Bookworm/Bookworm/View/Components/NavigationStackButtonStyle.swift
@@ -0,0 +1,8 @@
+//
+//  NavigationStackButtonStyle.swift
+//  Bookworm
+//
+//  Created by Isaque da Silva on 16/03/24.
+//
+
+import Foundation
