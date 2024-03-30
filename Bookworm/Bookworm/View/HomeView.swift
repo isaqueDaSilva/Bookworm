438329bc05bc601231ac2579c01da1a90815d524
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2024-03-30 20:34:11 -0300

    I added a new option for displaying books with a list view.

--- Bookworm/Bookworm/View/HomeView.swift
+++ Bookworm/Bookworm/View/HomeView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct HomeView: View {
     // MARK: - View Properties
+    @AppStorage("displaying_mode") var displayingMode: DisplayingMode = .icons
+    
     @Namespace private var transition
+    @Namespace private var transitionDisplayingMode
     @StateObject private var viewModel: HomeViewModel
     let colums = [GridItem(.adaptive(minimum: 150), spacing: 10, alignment: .top)]
     
     // MARK: - View
     var body: some View {
-        // START: NAV
         NavigationStack {
             Group {
                 if viewModel.books.isEmpty {
@@ -33,32 +35,32 @@ struct HomeView: View {
                 }
             }
             .navigationTitle("Bookworm")
-            .onAppear {
-                // Fetch Books saved in Core Data:
-                // When this View appears, the search for saved books
-                // will be carried out, and after that these results
-                // will be displayed on the screen for the user.
-                viewModel.fetchBooks()
-            }
             .toolbar {
-                Button {
-                    // Make the 'showingAddNewBook' as true
-                    // and displat the BookFormView.
+                HStack {
+                    Button {
+                        viewModel.showingAddNewBook = true
+                    } label: {
+                        Icons.plusCircle.systemImage
+                    }
                     
-                    viewModel.showingAddNewBook = true
-                } label: {
-                    Icons.plus.systemImage
+                    Menu {
+                        Picker("", selection: $displayingMode) {
+                            ForEach(DisplayingMode.allCases, id: \.id) {
+                                Label($0.rawValue, systemImage: $0.systemImageName)
+
+                            }
+                        }
+                    } label: {
+                        Icons.line3.systemImage
+                    }
+
                 }
             }
             .navigationDestination(for: Book.self) { book in
                 BookDetailView(storage: viewModel.storage, book: book)
             }
             .sheet(isPresented: $viewModel.showingAddNewBook) {
-                BookFormView(storage: viewModel.storage) {
-                    withAnimation(.spring.delay(0.1)) {
-                        viewModel.fetchBooks()
-                    }
-                }
+                BookFormView(storage: viewModel.storage)
             }
             .alert(viewModel.alertTitle, isPresented: $viewModel.showingAlert) {
             } message: {
@@ -66,11 +68,8 @@ struct HomeView: View {
             }
             
         }
-        // END: NAV
     }
     
-    /// This View shows the entire collection of books that the user has.
-    /// - Parameter storage: The type that contains the default container and viewContext types, of Core Data.
     init(storage: Storage) {
         _viewModel = StateObject(wrappedValue: HomeViewModel(storage: storage))
     }
@@ -81,9 +80,51 @@ struct HomeView: View {
 extension HomeView {
     @ViewBuilder
     private func BookListView() -> some View {
-        // START: SCROLL
+        Group {
+            switch displayingMode {
+            case .icons:
+                GridHomeView()
+            case .list:
+                ListHomeView()
+            }
+        }
+    }
+}
+
+// MARK: - List Home View
+
+extension HomeView {
+    @ViewBuilder
+    func ListHomeView() -> some View {
+        List(viewModel.books) { book in
+            NavigationLink(value: book) {
+                Cover(
+                    coverImage: book.coverImage,
+                    title: book.wrappedTitle,
+                    width: 75,
+                    height: 100
+                )
+                VStack(alignment: .leading) {
+                   Description(
+                    title: book.wrappedTitle,
+                    author: book.wrappedAuthorName
+                   )
+                }
+            }
+            .swipeActions {
+                DeleteBook(book, titleHidden: false)
+            }
+        }
+        .listStyle(.plain)
+    }
+}
+
+// MARK: - Grid Home View
+
+extension HomeView {
+    @ViewBuilder
+    func GridHomeView() -> some View {
         ScrollView {
-            // START: LazyVGrid
             LazyVGrid(columns: colums, spacing: 20) {
                 ForEach(viewModel.books) { book in
                     NavigationLink(value: book) {
@@ -95,20 +136,7 @@ extension HomeView {
                             .shadow(color: .black.opacity(0.15), radius: 5, x: 5, y: 5)
                             .shadow(color: .black.opacity(0.15), radius: 5, x: -5, y: -5)
                             .contextMenu {
-                                Button(
-                                    "Delete Book",
-                                    systemImage: Icons.trash.rawValue,
-                                    role: .destructive
-                                ) {
-                                    // When this button is pressed,
-                                    // the book will be permanently deleted
-                                    // and will disappear from the screen
-                                    // with the spring animmation.
-                                    
-                                    withAnimation(.spring.delay(0.65)) {
-                                        viewModel.deleteBook(book)
-                                    }
-                                }
+                                DeleteBook(book, titleHidden: true)
                             }
                             
                             Description(
@@ -121,9 +149,26 @@ extension HomeView {
                     .navigationStackButtonStyle()
                 }
             }
-            // END: LazyVGrid
         }
-        // END: SCROLL
+    }
+}
+
+// MARK: - Delete Book Button
+
+extension HomeView {
+    @ViewBuilder
+    func DeleteBook(_ book: Book, titleHidden: Bool) -> some View {
+        Button(role: .destructive) {
+            withAnimation(.spring.delay(0.65)) {
+                viewModel.deleteBook(book)
+            }
+        } label: {
+            Label(
+                titleHidden ? "Delete Book" : "",
+                systemImage: Icons.trash.rawValue
+            )
+                
+        }
     }
 }
 
