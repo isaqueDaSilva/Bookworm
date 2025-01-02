9e553db9a3f9b5e151ae192aab84867c4ecde9f9
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-01-02 20:44:46 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Isaque da Silva on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookwormTests/BookwormTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormTests.swift
+//  BookwormTests
+//
+//  Created by Isaque da Silva on 1/2/25.
+//
+
+import Testing
+@testable import Bookworm
+
+struct BookwormTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
